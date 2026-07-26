5e84a782244168b00c48eccf77676e8d2c762a05
Gaurav Trivedi <119726457+g7-aurav@users.noreply.github.com>
2026-07-26 17:33:04 +0530

    Delete sql_queries directory

--- sql_queries/script_activity_query.sql
+++
@@ -1,8 +0,0 @@
--- Use this query in script activity under ForEach
-
-SELECT COUNT(*) AS tgt_count
-FROM OPENROWSET(
-    BULK 'bronze/@{item().SchemaName}/@{item().TableName}/*.parquet',
-    DATA_SOURCE = 'FintechDataExternal',
-    FORMAT = 'PARQUET'
-) AS rows;
